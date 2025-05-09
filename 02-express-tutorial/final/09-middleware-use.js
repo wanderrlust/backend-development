fff0cb8cd4cfb14d40ef3a875c72b5e5a3b95526
wanderrlust <kuchmakivan@gmail.com>
2025-05-09 17:59:28 +0300

    completed middleware tasks

---
+++ 02-express-tutorial/final/09-middleware-use.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+app.use([authorize, logger]);
+
+app.get('/', (req, res) => {
+	res.send('Home');
+});
+app.get('/about', (req, res) => {
+	res.send('About');
+});
+app.get('/api/products', (req, res) => {
+	res.send('Products');
+});
+app.get('/api/items', (req, res) => {
+	console.log(req.user);
+	res.send('Items');
+});
+
+app.listen(5500, () => {
+	console.log('server is listening on port 5500');
+});
