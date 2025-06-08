039aaceb113c859e891676e3b9cf44fec1131d61
wanderrlust <kuchmakivan@gmail.com>
2025-06-08 20:52:38 +0300

    initial routing setup

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+//midleware
+
+app.use(express.json());
+
+
+//routes
+
+app.get('/', (req, res) => {
+	res.send('Task Manager App');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(
+	port,
+	console.log(`Server is running on http://localhost:${port}...`)
+);
