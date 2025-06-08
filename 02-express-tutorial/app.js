1be64bce5e78368b29ff46b1fdfef2cee614a1df
wanderrlust <kuchmakivan@gmail.com>
2025-06-08 19:05:05 +0300

    tasks about http methods from postman are completed

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -20,12 +20,57 @@ app.post('/api/people', (req, res) => {
 	res.status(201).json({ success: true, person: name });
 });
 
+app.post('/api/postman/people', (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res
+			.status(400)
+			.json({ success: false, msg: 'please provide name value' });
+	}
+	res.status(201).json({ success: true, data: [...people, name] });
+});
+
 app.post('/login', (req, res) => {
 	const { name } = req.body;
 	if (name) {
 		return res.status(200).send(`Welcome ${name}`);
 	}
 	res.status(401).send('Please provide credentials');
+});
+
+app.put('/api/people/:id', (req, res) => {
+	const { id } = req.params;
+	const { name } = req.body;
+	if (!name) {
+		return res
+			.status(400)
+			.json({ success: false, msg: 'please provide name value' });
+	}
+	const person = people.find((person) => person.id === Number(id));
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `no person found with id ${id}` });
+	}
+	const newPeople = people.map((person) => {
+		if (person.id === Number(id)) {
+			person.name = name;
+		}
+		return person;
+	});
+	res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+	const { id } = req.params;
+	const person = people.find((person) => person.id === Number(id));
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `no person found with id ${id}` });
+	}
+	const newPeople = people.filter((person) => person.id !== Number(id));
+	res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5500, () => {
